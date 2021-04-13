8940fe96f4814e13362b98db9703d4520a5f1163
Edwin Lugo <lugoedwin96@gmail.com>
2021-04-12 17:09:36 -0700

    Fetch random data while having a loading prompt

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [data, setData] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/adhithiravi/React-Hooks-Examples/master/testAPI.json')
+    .then(response => response.json() )
+    .then((data) => { setData(data.title) })
+    .catch((error) => { console.log(error) })
+    .finally(() => setLoading(false));
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    {isLoading ? <Text>Is loading ...</Text> : <Text>This is the a + {data}</Text>}
       <StatusBar style="auto" />
     </View>
   );
